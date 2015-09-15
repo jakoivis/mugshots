870f61d8d3cc9b90dfa982f5eeb46358b774ba72
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-09-15 22:05:57 +0300

    Fix positioning

--- src/components/face/facePartPositioningUtil.js
+++ src/components/face/facePartPositioningUtil.js
@@ -3,76 +3,58 @@
 
 var FacePartPositioningUtil = {
 
-    setRandomNosePosition: function(stacks)
-    {
+    setRandomNosePosition: function(stacks) {
+
         stacks.nose.current().setRandomYPosition();
     },
 
-    setRandomMouthPosition: function(stacks)
-    {
+    setRandomMouthPosition: function(stacks) {
+
         changeMouthIfItDoesntFit(stacks);
 
-    //     var sizeRatio = mouth.height / getMouthRange();
-    //     var offset = Math.round(Math.random() * (getMouthAvailableMovement() * sizeRatio));
-    //     mouth.bitmap.y = Math.round(nose.bottom - mouth.top + offset);
-
         // freely position mouth in the designated area
-        var mouthFacePart = stacks.mouth.current();
-        var noseFacePart = stacks.nose.current();
-        var mouthAvailableSpace = getMouthAvailableSpace(stacks);
+        var mouth = stacks.mouth.current();
+        var nose = stacks.nose.current();
         var mouthAvailableMovement = getMouthAvailableMovement(stacks);
+        var mouthTopMin = nose.bottom;
+        var mouthTopMax = mouthTopMin + mouthAvailableMovement;
 
-        console.log("mouthAvailableMovement: " + mouthAvailableMovement);
-
-        // var sizeRatio = mouthFacePart.height / mouthAvailableSpace;
-        // var offset = Math.round(Math.random() * (mouthAvailableMovement * sizeRatio));
-        // console.log(mouthFacePart.top);
-        // mouthFacePart.bitmap.y = Math.round(noseFacePart.bottom - mouthFacePart.top + offset);
-        var mouthMin = noseFacePart.bottom;
-        var mouthMax = mouthMin + mouthAvailableMovement;
-
-        var x = getRandomInt(mouthMin, mouthMax);
-
-        console.log(x);
+        mouth.top = getRandomInt(mouthTopMin, mouthTopMax);
     }
 };
 
-function getRandomInt(min, max)
-{
-    return Math.floor(Math.random() * (max - min)) + min;
-}
+function changeMouthIfItDoesntFit(stacks) {
 
-function changeMouthIfItDoesntFit(stacks)
-{
-    while(canMouthFit(stacks))
-    {
+    while(canMouthFit(stacks)) {
+
+        console.log("didn't fit, change image");
         stacks.mouth.next();
-        // console.log("Changed mouth since it didn't fit in the available area");
     }
 }
 
-function canMouthFit(stacks)
-{
-    var range = getMouthAvailableMovement(stacks);
+function canMouthFit(stacks) {
 
-    // console.log("mouth avail. movement range Y: " + range);
-
-    return range < 0;
+    return getMouthAvailableMovement(stacks) < 0;
 }
 
-function getMouthAvailableMovement(stacks)
-{
+function getMouthAvailableMovement(stacks) {
+
     var mouth = stacks.mouth.current();
 
     return getMouthAvailableSpace(stacks) - mouth.height;
 }
 
-function getMouthAvailableSpace(stacks)
-{
+function getMouthAvailableSpace(stacks) {
+
     var nose = stacks.nose.current();
     var background = stacks.background.current();
 
     return background.bottom - nose.bottom;
+}
+
+function getRandomInt(min, max) {
+
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 module.exports = FacePartPositioningUtil;
