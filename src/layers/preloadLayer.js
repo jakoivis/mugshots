3068a3db437893fe16376fd0e077449e00f146b2
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-09-17 22:02:13 +0300

    Create shadow for spinner

--- src/layers/preloadLayer.js
+++ src/layers/preloadLayer.js
@@ -21,50 +21,72 @@ function PreloadLayer(options) {
     var canvas;
     var stage;
     var spinner;
+    var spinnerShadow;
+    var spinnerShadowMask;
+    var spinnerSettings;
 
-    var spinnerSettings = {
-
-        fadeInStart: {
-            alpha: 0,
-            outerRadius: 60,
-            innerRadius: 40,
-            rotationSpeed: 0.1,
-            tickWidth: 8,
-            tickColor: 0x444444,
-            tickHighLightColor: 0xEEEEEE,
-            y: 100
-        },
-
-        fadeInEnd: {
-            alpha: 0.5,
-            outerRadius: 20,
-            innerRadius: 15,
-            rotationSpeed: 0.03,
-            tickWidth: 4,
-            y: 620
-        },
-
-        fadeOutEnd: {
-            alpha: 0,
-            outerRadius: 60,
-            innerRadius: 40,
-            rotationSpeed: 0.1,
-            tickWidth: 8
-        }
-    };
 
     function init() {
 
         initOptions();
+        spinnerSettings = createSpinnerSettings();
 
         stage = new createjs.Stage(canvas);
 
         amplify.subscribe(TOPICS.PRELOAD_COMPLETE, removeSpinner);
 
-        createjs.Ticker.setFPS(60);
+        createjs.Ticker.setFPS(30);
         createjs.Ticker.on("tick", timerTickHandler);
 
         showSpinner();
+        showSpinnerShadow();
+    }
+
+    function createSpinnerSettings() {
+
+        var x = 200;
+        var y = 200;
+        var diameter = 44;
+        var radius = diameter / 2;
+        var shadowOffsetY = diameter + 4;
+
+        return {
+            x: x,
+            y: y,
+            radius: radius,
+            diameter: diameter,
+            shadowX: x,
+            shadowY: y + shadowOffsetY,
+
+            fadeInStart: {
+                numberOfTicks: 16,
+                alpha: 0,
+                outerRadius: 60,
+                innerRadius: 40,
+                rotationSpeed: 0.1,
+                tickWidth: 8,
+                tickColor: 0xEEEEEE,
+                tickHighLightColor: 0x444444,
+                x: radius,
+                y: radius
+            },
+
+            fadeInEnd: {
+                alpha: 0.5,
+                outerRadius: 20,
+                innerRadius: 15,
+                rotationSpeed: 0.03,
+                tickWidth: 4
+            },
+
+            fadeOutEnd: {
+                alpha: 0,
+                outerRadius: 60,
+                innerRadius: 40,
+                rotationSpeed: 0.1,
+                tickWidth: 8
+            }
+        };
     }
 
     function initOptions() {
@@ -81,7 +103,7 @@ function PreloadLayer(options) {
 
             canvas.width = options.width;
 
-            spinnerSettings.fadeInStart.x = options.width >> 1;
+            // spinnerSettings.fadeInStart.x = options.width >> 1;
         }
 
         if(options.height) {
@@ -94,6 +116,9 @@ function PreloadLayer(options) {
     function timerTickHandler(event) {
 
         spinner.update();
+        spinnerShadow.update();
+        spinnerShadowMask.updateCache();
+        spinnerShadow.container.updateCache();
         stage.update(event);
     }
 
@@ -102,6 +127,8 @@ function PreloadLayer(options) {
         spinner = new Spinner(spinnerSettings.fadeInStart);
 
         stage.addChild(spinner.container);
+        spinner.container.x = spinnerSettings.x;
+        spinner.container.y = spinnerSettings.y;
 
         createjs.Tween
             .get(spinner)
@@ -109,12 +136,82 @@ function PreloadLayer(options) {
                 createjs.Ease.bounceOut);
     }
 
+    function showSpinnerShadow() {
+
+        createGlow();
+
+        spinnerShadow = createShadow();
+        spinnerShadowMask = createShadowMask();
+
+        spinnerShadow.container.filters = [new createjs.AlphaMaskFilter(spinnerShadowMask.cacheCanvas)];
+        spinnerShadow.container.cache(0, 0, spinnerSettings.diameter, spinnerSettings.diameter);
+
+        stage.addChild(spinnerShadow.container);
+
+
+        function createGlow() {
+
+            var glow = new createjs.Shape();
+            var r = spinnerSettings.radius;
+            var x = spinnerSettings.x + r;
+            var y = spinnerSettings.y + r;
+            var glowRadius = r + 10;
+
+            glow.graphics.beginRadialGradientFill(
+                    ["rgba(255, 255, 255, 0.5)", "rgba(255, 255, 255, 0)"],
+                    [0, 1], x, y, 0, x, y, glowRadius)
+                .drawCircle(x, y, glowRadius);
+
+            stage.addChild(glow);
+        }
+
+        function createShadow() {
+
+            var spinner = new Spinner(spinnerSettings.fadeInStart);
+
+            var container = spinner.container;
+            container.regY = spinnerSettings.diameter;
+            container.scaleY = -1;
+            container.x = spinnerSettings.shadowX;
+            container.y = spinnerSettings.shadowY;
+
+            createjs.Tween
+                .get(spinner)
+                .to(spinnerSettings.fadeInEnd, 1000, createjs.Ease.bounceOut);
+
+            return spinner;
+        }
+
+        function createShadowMask() {
+
+            var mask = new createjs.Shape();
+            var d = spinnerSettings.diameter;
+
+            mask.graphics.beginLinearGradientFill(
+                    ["rgba(0, 0, 0, 0)", "rgba(0, 0, 0, 0.4)"],
+                    [0, 1], 0, 0, 0, d);
+
+            mask.graphics.drawRect(0, 0, d, d);
+
+            mask.cache(0, 0, d, d);
+
+            stage.addChild(mask);
+
+            return mask;
+        }
+
+        // glow.graphics.beginLinearGradientFill(
+                // ["rgba(255, 255, 255, 0.4)", "rgba(255, 255, 255, 0)"],
+                // [0, 1], x, y, x, y+d-10);
+        // glow.graphics.drawRect(x, y, 46, 46);
+    }
+
     function removeSpinner() {
 
-        createjs.Tween
-            .get(spinner)
-            .to(spinnerSettings.fadeOutEnd, 1000,
-                createjs.Ease.bounceOut);
+        // createjs.Tween
+        //     .get(spinner)
+        //     .to(spinnerSettings.fadeOutEnd, 1000,
+        //         createjs.Ease.bounceOut);
     }
 
     init();
