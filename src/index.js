2ea1dc93f203044c6a40092fcdc5c135fdb078df
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-09-19 22:01:41 +0300

    Clean index js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 
 "use strict";
 
-// easeljs is not commonjs module. import it once
+// createjs is not commonjs module. import it once
 window.createjs = window.createjs || {};
-window.easeljs = createjs;
 require("easeljs");
 require("tweenjs");
 
@@ -11,27 +10,17 @@ var PreloadService = require("./services/preloadService.js");
 var PreloadLayer = require("./layers/preloadLayer.js");
 var FaceLayer = require("./layers/faceLayer.js");
 var DebugControlsLayer = require("./layers/debugControlsLayer.js");
-// var OverlayLayer = require("./layers/overlayLayer.js");
 
-var layerWidth = 600;
-var layerHeight = 800;
-
-// new FaceLayer({
-//     target: "face",
-//     width: layerWidth,
-//     height: layerHeight
-// });
+new FaceLayer({
+    target: "face"
+});
 
 new PreloadLayer({
-    target: "loader",
-    pointerEvents: false
+    target: "loader"
 });
 
 new DebugControlsLayer({
     target: "debugControls"
 });
-
-
-// new OverlayLayer();
 
 PreloadService.load();
