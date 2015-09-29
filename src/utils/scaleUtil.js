f22afa4f6033ac2ad0778cea9387f4748421dcfb
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-09-29 21:18:15 +0300

    ScaleUtil

---
+++ src/utils/scaleUtil.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var ScaleUtil = {
+
+    fit: function (contentWidth, contentHeight, containerWidth, containerHeight) {
+
+        return null;
+    }
+
+    /**
+     * Scale the dimensions to fill container dimensions while maintaining aspect ratio.
+     *
+     * @param {number} contentWidth Width that will be scaled
+     * @param {number} contentHeight Height that will be scaled
+     * @param {number} containerWidth Width of the container that is filled
+     * @param {number} containerHeight Height of the container that is filled
+     *
+     * @return Object containing the width and height dimensions and x and y offsets for centering the scaled dimension relative to the container.
+     */
+    fill: function(contentWidth, contentHeight, containerWidth, containerHeight) {
+
+        var containerAspect = containerWidth / containerHeight;
+        var aspect = contentWidth / contentHeight;
+        var result;
+
+        if(aspect >= containerAspect) {
+
+            // content is wider than container
+
+            var width = containerHeight * aspect;
+
+            result = {
+                x: -((width>>1) - (containerWidth>>1)),
+                y: 0,
+                width: width,
+                height: containerHeight
+            };
+
+        } else {
+
+            // container is wider than content
+
+            var height = Math.round(containerWidth / aspect);
+
+            result = {
+                x: 0,
+                y: -((h>>1) - (containerHeight>>1)),
+                width: containerWidth,
+                height: height
+            };
+        }
+
+        return result;
+    }
+}
+
+module.exports = ScaleUtil;
