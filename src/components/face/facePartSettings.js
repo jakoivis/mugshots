368d9e53b405bfade236e328f6373dae51bf0354
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-10-12 22:08:39 +0300

    Change debug bounds alpha

--- src/components/face/facePartSettings.js
+++ src/components/face/facePartSettings.js
@@ -1,6 +1,9 @@
 
 "use strict";
 
+var alpha1 = 0.4;
+var alpha2 = 0.1;
+
 var FacePartSettings = {
     BITMAP_ALPHA_TOLERANCE: 210,
 
@@ -21,40 +24,40 @@ var FacePartSettings = {
         defaultRect: {x:187, y:279, width:164, height:215},
         rangeY: {min: -20, max: 20},
         rangeX: {min: 0, max: 0},
-        debugColor1: "#00FF00",
-        debugColor2: "#90FF90"
+        debugColor1: "rgba(0,255,0,"+alpha1+")",
+        debugColor2: "rgba(125,255,125,"+alpha2+")"
     },
 
     mouth: {
         defaultRect: {x:147, y:435, width:273, height:275},
         rangeY: {min: 0, max: 0},
         rangeX: {min: 0, max: 0},
-        debugColor1: "#FF0000",
-        debugColor2: "#FF9090"
+        debugColor1: "rgba(255,0,0,"+alpha1+")",
+        debugColor2: "rgba(255,125,125,"+alpha2+")"
     },
 
     chin: {
         defaultRect: {x:82, y:254, width:397, height:401},
         rangeY: {min: 0, max: 0},
         rangeX: {min: 0, max: 0},
-        debugColor1: "#0000FF",
-        debugColor2: "#9090FF"
+        debugColor1: "rgba(0,0,255,"+alpha1+")",
+        debugColor2: "rgba(125,125,255,"+alpha2+")"
     },
 
     lefteye: {
         defaultRect: {x:80, y:225, width:215, height:192},
         rangeY: {min: -20, max: 30},
         rangeX: {min: 0, max: 0},
-        debugColor1: "#FFFF00",
-        debugColor2: "#FFFF90"
+        debugColor1: "rgba(255,255,0,"+alpha1+")",
+        debugColor2: "rgba(255,255,125,"+alpha2+")"
     },
 
     righteye: {
         defaultRect: {x:260, y:224, width:214, height:193},
         rangeY: {min: -15, max: 30},
         rangeX: {min: 0, max: 0},
-        debugColor1: "#00FFFF",
-        debugColor2: "#90FFFF"
+        debugColor1: "rgba(0,255,255,"+alpha1+")",
+        debugColor2: "rgba(125,255,255,"+alpha2+")"
     }
 };
 
