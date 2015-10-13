8203bcfd720c1f8213d8a2d5d5b9bd1a17901583
Jarmo Koivisto <jarmo.kvst@gmail.com>
2015-10-13 21:02:46 +0300

    Add debug layer for testing face

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ var PreloadService = require("./services/preloadService.js");
 var PreloadLayer = require("./layers/preloadLayer.js");
 var ApplicationLayer = require("./layers/ApplicationLayer.js");
 var DebugControlsLayer = require("./layers/debugControlsLayer.js");
+var DebugFaceLayer = require("./layers/debugFaceLayer.js");
 
 createjs.Ticker.setFPS(30);
 
@@ -21,8 +22,13 @@ new PreloadLayer({
     target: "loader"
 });
 
-new DebugControlsLayer({
-    target: "debugControls"
-});
+// new DebugFaceLayer({
+//     target: "application"
+// });
+
+// new DebugControlsLayer({
+//     target: "debugControls"
+// });
+
 
 PreloadService.load();
---
+++ src/layers/debugFaceLayer.js
@@ -0,0 +1,97 @@
+
+"use strict";
+
+var BasicLayer = require("../layers/basicLayer.js");
+var Face = require("../components/face/Face.js");
+
+var amplify = require("amplify").amplify;
+var Topics = require("../topics.js");
+
+function ApplicationLayer(options) {
+
+    var me = this;
+
+    var face;
+
+    me.initialize = function() {
+
+        face = new Face();
+
+        initTopics();
+    };
+
+    me.onTick = function(event) {
+
+        me.stage.update(event);
+    };
+
+    me.onApplicationStart = function() {
+
+        addClickHandler();
+
+        face.x = me.stageWidth/2;
+        face.y = me.stageHeight/2;
+
+        face.update();
+
+        me.stage.addChild(face);
+    };
+
+    function addClickHandler() {
+        face.on("click", function() {
+            face.setRandomFaceParts();
+            face.setRandomPositions();
+            face.update();
+        });
+    }
+
+    function initTopics() {
+
+        amplify.subscribe(Topics.NEXT_BACKGROUND, function() {
+
+            face.stacks.background.next();
+            face.setRandomPositions();
+            face.update();
+        });
+
+        amplify.subscribe(Topics.NEXT_NOSE, function() {
+
+            face.stacks.nose.next();
+            face.setRandomPositions();
+            face.update();
+        });
+
+        amplify.subscribe(Topics.NEXT_LEFT_EYE, function() {
+
+            face.stacks.lefteye.next();
+            face.setRandomPositions();
+            face.update();
+        });
+
+        amplify.subscribe(Topics.NEXT_RIGHT_EYE, function() {
+
+            face.stacks.righteye.next();
+            face.setRandomPositions();
+            face.update();
+        });
+
+        amplify.subscribe(Topics.NEXT_MOUTH, function() {
+
+            face.stacks.mouth.next();
+            face.setRandomPositions();
+            face.update();
+        });
+
+        amplify.subscribe(Topics.RANDOM_POSITIONS, function() {
+
+            face.setRandomPositions();
+            face.update();
+        });
+    }
+
+    me.BasicLayer_constructor(options);
+}
+
+createjs.extend(ApplicationLayer, BasicLayer);
+
+module.exports = createjs.promote(ApplicationLayer, "BasicLayer");
